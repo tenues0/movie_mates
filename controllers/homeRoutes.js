3787e278b1819156d7420ab1e84e8818f3dd12ef
yippeejimmee <77139684+yippeejimmee@users.noreply.github.com>
2022-07-10 18:13:39 -0700

    nothing

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -59,6 +59,7 @@ router.get('/movies/:id', async (req, res) => {
 
 // Use withAuth middleware to prevent access to route
 router.get('/profile', withAuth, async (req, res) => {
+  console.log("getting into profile");
   try {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
