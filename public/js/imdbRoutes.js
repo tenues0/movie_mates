2e57060181a2144663e4e26ca5d69b4574f824ab
yippeejimmee <77139684+yippeejimmee@users.noreply.github.com>
2022-07-12 18:56:09 -0700

    imdb routes, added event delegation

--- public/js/imdbRoutes.js
+++ public/js/imdbRoutes.js
@@ -6,6 +6,7 @@
 
 var movieList = [];
 var movieListStringified = [];
+var movieInfo;
 
 function movieLookup(event) {
     event.preventDefault();
@@ -13,7 +14,7 @@ function movieLookup(event) {
     const movie = document.querySelector('#searchMovie').value.trim();
     console.log("running imdb?")
     console.log(movie);
-    fetch(`https://imdb-api.com/en/API/SearchMovie/k_u4zar1it/${movie}`)
+    fetch(`https://imdb-api.com/en/API/SearchMovie/k_df1r50ia/${movie}`)
         .then(response => response.json())
         .then(function (data) {
             console.log(data);
@@ -21,6 +22,7 @@ function movieLookup(event) {
             //JIMMY note: Trying to get the movie data to create cards to show onto page
             data.results.forEach(function (item) {
                 movies = {
+                    imdb_id: item.id,
                     title: item.title,
                     poster: item.image,
                 }
@@ -36,16 +38,35 @@ function movieLookup(event) {
 
                 movieSearchList.innerHTML = `
                 <div class="seperatePosters">
+                    <h6 class="imdbid">${movieIndex.imdb_id}</h6>
                     <img class="card-img" src="${movieIndex.poster}" alt="Poster for ${movieIndex.title}">
                     <div class="card-body">
                     <h5 class="card-title">${movieIndex.title}</h5>
                     <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    <button class="btn search-btn" id="btn-${i}" type="submit">Select Movie</button>
                     </div>
                 </div>
                 `
                 document.querySelector("#displayMovieResults").append(movieSearchList);
 
+            }
+            var movieParent = document.querySelector('#displayMovieResults');
+            movieParent.onclick = function (e) {
+                console.log("CLICKED");
+                let target = e.target;
+                // movieInfo = target.parentElement.parentElement;
+                var imdb = document.querySelector('.imdbid').innerHTML;
+                console.log(imdb);
+                if (target.className != 'btn search-btn') return;
+                console.log(target.className);
+                // if (target.class != 'seperatePosters') return;
+                highlight(target);
+            };
+
+            function highlight(highlightMovie) {
+                selectedMovie = highlightMovie;
+                console.log(movieInfo);
+                selectedMovie.classList.add('highlight')
             }
 
         })
