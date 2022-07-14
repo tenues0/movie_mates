f4d8de8c715c83b306533ed409ec5c8c6e0fada3
yippeejimmee <77139684+yippeejimmee@users.noreply.github.com>
2022-07-13 18:03:13 -0700

    fixed links

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -13,26 +13,25 @@ router.get('/', async (req, res) => {
   try {
     const postData = await Movies.findAll({
       attributes: ["id", "movie_review", "post_content", "date_created", "user_id"],
-      include: [
-        {
+      include: [{
           model: Ratings,
-                    attributes: [
-                        "id",
-                        "rating_content",
-                        "movies_id",
-                        "user_id",
-                        "date_created",
-                    ],
-                    include: {
-                        model: User,
-                        attributes: ["username"],
-                    }, 
+          attributes: [
+            "id",
+            "rating_content",
+            "movies_id",
+            "user_id",
+            "date_created",
+          ],
+          include: {
+            model: User,
+            attributes: ["username"],
+          },
         },
         {
-        model: User,
-        attributes: ['username'],
-      },
-     ]
+          model: User,
+          attributes: ['username'],
+        },
+      ]
     });
     //serialize the data//
     const posts = postData.map((post) =>
@@ -40,7 +39,7 @@ router.get('/', async (req, res) => {
         plain: true
       })
     );
-      //return Handlebars template to homepage
+    //return Handlebars template to homepage
     res.render('homepage', {
       posts,
       logged_in: req.session.logged_in,
@@ -57,60 +56,66 @@ router.get('/', async (req, res) => {
 });
 
 //this is the route to add comment to a post; NEED TO WORK ON THIS//
-router.get('/post/:id', async (req, res) => {
+router.get('/post/:id', withAuth, async (req, res) => {
   console.log("getting post/id")
   console.log(req.params.id)
   console.log(req.body)
   try {
+    console.log(req.params.id);
     const moviesData = await Movies.findOne({
       where: {
         id: req.params.id,
       },
-    attributes: ["id", "post_content", "movie_review", "date_created"],    
-    include: [
-        {
-            model: Ratings,
-            attributes: [
-                "id",
-                "rating_content",
-                "movies_id",
-                "user_id",
-                "date_created",
-            ],
-            include: {
-                model: User,
-                attributes: ["username"],
-            },
-        },
-        {
+      attributes: ["id", "post_content", "movie_review", "date_created"],
+      include: [{
+          model: Ratings,
+          attributes: [
+            "id",
+            "rating_content",
+            "movies_id",
+            "user_id",
+            "date_created",
+          ],
+          include: {
             model: User,
             attributes: ["username"],
+          },
         },
-    ],
-})
- // if no data is found, return 404 and message
- if (!moviesData) {
-  res.status(404).json({ message: "error here at get by id" });
-  return;
-}
-// By default Sequelize returns lots of metadata
-// To turn medatada off, we use the plain: true option
-const post = moviesData.get({ plain: true });
-// render the Handlebars view here
-res.render("post", {
-  post,
-  logged_in: req.session.logged_in,
-  username: req.session.username
-});
-} catch(err) {
-console.log(err);
-// returns a '500 Internal Server Error' response
-res.status(500).json(err);
-};
+        {
+          model: User,
+          attributes: ["username"],
+        },
+      ],
+    })
+    // if no data is found, return 404 and message
+    if (!moviesData) {
+      res.status(404).json({
+        message: "error here at get by id"
+      });
+      return;
+    }
+    // By default Sequelize returns lots of metadata
+    // To turn medatada off, we use the plain: true option
+
+    const post = moviesData.get({
+      plain: true
+    });
+    console.log("****", post);
+    // render the Handlebars view here
+    res.render("post", {
+      ...post,
+      logged_in: req.session.logged_in,
+      username: req.session.username
+    });
+  } catch (err) {
+    console.log(err);
+    // returns a '500 Internal Server Error' response
+    res.status(500).json(err);
+  };
 });
 
-    
-    
+
+
 
 // GET '/login' (login form)
 router.get('/login', (req, res) => {
